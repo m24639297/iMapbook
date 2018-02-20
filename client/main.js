bbc767c94deb80bf751100139e89f14682e61e58
m24639297 <m24639297@gmail.com>
2018-02-20 23:59:34 +0800

    main page prototype

--- client/main.js
+++ client/main.js
@@ -1,7 +1,7 @@
 var current_location = [25.020418, 121.537630]
 var lat = current_location[0];
 var lng = current_location[1];
-var map = L.map('mapid').setView(current_location, 15);
+var map = L.map('mapid',{doubleClickZoom : false}).setView(current_location, 15);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
     maxZoom: 18,minZoom: 8,
@@ -10,7 +10,8 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=p
         'Imagery c <a href="http://mapbox.com">Mapbox</a>',
     id: 'mapbox.streets'
 }).addTo(map);
-map.setView(current_location,16)
+map.setView(current_location,15)
+
 
 board_list = ["board1", "board2", "board3"]
 current_board = ""
@@ -22,6 +23,9 @@ lngW = map.getBounds().getWest();
 latN = map.getBounds().getNorth();
 latS = map.getBounds().getSouth();
 show_all = true
+alldata = [{"event_id":"001","event_name":"Fist event", "event_type":"type1","location":"NTU", "existing":"true", "info_list":[{"report_time":"2017/01/03", "user_id":"user000", "report_id":"0001", "description":"there's a hole!!!"},{"report_time":"2017/01/04", "user_id":"HELLO", "report_id":"0002", "description":" HOLEEEEEEE hole!!!"}]}]
+
+console.log(alldata[0]["info_list"])
 
 //get board list and show on website
 //board_list = ...??
@@ -29,17 +33,24 @@ for (var i = 0; i < board_list.length; i++) {
     document.getElementById("board-select").innerHTML += `<option> ${board_list[i]} </option>`
 }
 
+function board_refresh(){
+    current_board = document.getElementById('board-select').value
+    console.log(`current board is ${current_board}`)
+    update_type()
+    show_icon()
+
+}
+
 //get type_list of this board
 function update_type() {
     document.getElementById("type-content").innerHTML = ""
     check_list = []
-    // get list from `/$('#board-select :selected').text();/data`
+    // get list (type_list) from current board (current_board)
     for (var i = 0; i < type_list.length; i++) {
         check_list.push(false)
         document.getElementById("type-content").innerHTML 
         += `<input type="checkbox" name="${type_list[i]}" value="${i}" onClick="type_change(${i})"> ${type_list[i]} <br>`;
     }
-    current_board = document.getElementById('board-select').value
 }
 
 // change check_list (T <-> F)
@@ -53,7 +64,7 @@ function set_time(){
     var now = new Date()
     var s = now.getTime()
     var t = document.getElementById("time-selector").value
-    console.log(t)
+    console.log(`t is ${t}`)
     switch(t){
         case '1 hr':
             time_interval = 1;
@@ -82,18 +93,22 @@ function set_time(){
     }
     s -= time_interval*3600000
     start_time = new Date(s)
-    console.log(start_time)
+    console.log(`data start from ${start_time}`)
+    show_icon()
 }
 
 // show all data or only existing data
 function set_show_all(){
-    showall = document.getElementById('show-finished-data').checked
+    show_all = document.getElementById('show-finished-data').checked
+    if(show_all) console.log("show all events")
+    else console.log("show only ongoing events")
+    show_icon()
 }
 
 //get evnets to show -> put marker -> set popup
 function show_icon(){
     for (var i = 0; i < type_list.length; i++) if(check_list[i]==true){{
-        // create requestURL: board=current_board; type=type_list[i]; all_data: show_all; range:(lngW,lngE)x(latS,latN); time_since:start_time
+        // create requestURL: board=current_board; type=type_list[i]; existing: show_all; range:(lngW,lngE)x(latS,latN); time_since:start_time
         requestURL = ""
         _show_icon(i,requestURL)
     }}
@@ -101,58 +116,72 @@ function show_icon(){
 
 function _show_icon(i,requestURL){
     var request = new XMLHttpRequest();
-    request.open('POST', requestURL);
+    request.open('GET', requestURL);
     request.responseType = 'json';
     request.send();
     request.onload = function(){
         var data_list = request.response
         for (var j = 0; j < data_list.length; j++) {
+            if(data_list[j]["existing"] == false && show_all == false){continue;}
             tmp_mark = L.marker([data_list[j]["lat"],data_list[j]["lng"]])
             tmp_mark.addTo(map)
-            tmp_mark.bindPopup(pop_content(
-                data_list[j]["event_id"],
-                data_list[j]["event_name"],
-                data_list[j]["event_type"],
-                data_list[j]["location"],
-                data_list[j]["existing"],
-                data_list[j]["info_list"]),{maxHeight: 300})
-            tmp_mark.on('click', function onClick_marker(e){
-                tmp_mark.openPopup(e.latlng())
-            })
+            tmp_mark.bindPopup(popup_content(data_list[j]),{maxHeight: 300})
+            // tmp_mark.on('click', function onClick_marker(e){
+            //     tmp_mark.openPopup(e.latlng())
+            // })
         }
     }
 }
+//popup_content = ""//`<div style="height: 80px "><div style="float: left;">${event_name} <br>${location}</div><div style="float: right;"><input type="button" id="edit-button" value="Edit"><input type="button" id="report-problem-button" value="Report"> </div></div><div><div style="float: left; width: 67%; min-height: 200px; border: solid;">${description}</div><div style="float: right; width: 32%; min-height: 200px; border: solid;">Hey dick</div></div><br><br><div><div style="height: 20px;"><div style="float: right;"><input type="button" id="show-all-button" value="Show all data"></div></div></div>`
 
-function pop_content(event_id,event_name,event_type,location,existing,info_list){
-
+function popup_content(_data){
+    s0 = ""
+    console.log(_data["existing"])
+    if(_data["existing"]=="true") {s0='<div style="height:280px">'}
+    else {console.log("allo");s0 =  '<div style="height:280px">'}
+    s0 +=
+    `<div style="height: 50px ">
+        <div style="float: left;">
+            ${_data["event_name"]} <br>
+            @${_data["location"]}
+        </div>
+        <div style="float: right;">
+            <input type="button" id="edit-button" value="Edit">
+            <input type="button" id="report-problem-button" value="Report"> 
+        </div>
+    </div>`
+    s0 += `<div><div style="float: left; width: 62%; min-height: 200px; max-height: 200px; border: solid;overflow:scroll">`
+    for (var i = 0; i < _data["info_list"].length; i++) {
+        s0 += `<div style="border: solid;">
+                ${_data["info_list"][i]["user_id"]}  ${_data["info_list"][i]["report_time"]} 
+                <div style="border: dashed; padding:2px;">
+                    ${_data["info_list"][i]["description"]}
+                </div></div><br>`
+    }
+    s0 += `</div>
+            <div style="float: right; width: 30%; min-height: 200px;max-height: 200px; border: solid;">
+                Photo?
+            </div>
+        </div><br><br></div>`
+    return s0;
 }
 
 // Ask current location and put current position marker
 
-marker = L.marker(current_location)
-marker.addTo(map)
-marker.bindPopup(
-    `<div style="height: 50px ">
-    <div style="float: left;">
-        {event_name} <br>
-        {location}
-    </div>
-    <div style="float: right;">
-        <input type="button" id="edit-button" value="Add">
-        <input type="button" id="report-problem-button" value="Report"> 
-    </div>
-</div>
-<div>
-    <div style="float: left; width: 50%; height:50px; border: solid;">
-        {description}
-    </div>
-    <div style="float: right; width: 30%;height:50px; border: solid;">
-        Photos here
-    </div>
-</div><br><br>
-<div>
-    <div style="height: 20px;">
-        <div style="float: right;"><input type="button" id="show-all-button" value="Show all data"></div>
-    </div>
-</div>`, {minWidth:270, maxWidth: 1000, maxHeight:170})
-marker.openPopup(current_location)
+function update_all_data(){
+    for (var i = 0; i < alldata.length; i++) {
+        document.getElementById("event-list-content").innerHTML += all_data_html(alldata[i])
+
+    }
+}
+
+function all_data_html(data){
+
+}
+
+mm=  L.marker(current_location)
+mm.addTo(map)
+mm.bindPopup(popup_content(alldata[0]),{maxHeight:300, minWidth:300})
+
+
+
